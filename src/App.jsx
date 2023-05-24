3d8679eaf5d590d2aa62b136b370c1277e57c4f4
haminem <ryuu.shushu@gmail.com>
2023-05-24 17:35:34 +0900

    update ButtonコンポーネントのPropsにStyleを追加

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import "./App.css";
 function App() {
 	return (
 		<>
-			<Button url="https://www.google.com/" context="Google" />
-			<Button url="https://www.youtube.com/" context="YouTube" />
-			<Button url="https://www.twitter.com/" context="Twitter" />
+			<Button url="https://www.google.com/" context="Google" style={{ backgroundColor: "red" }} />
+			<Button url="https://www.youtube.com/" context="YouTube" style={{ backgroundColor: "blue" }} />
+			<Button url="https://www.twitter.com/" context="Twitter" style={{ backgroundColor: "green" }} />
 		</>
 	);
 }
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import mergeStyle from "@/utils/mergeStyle";
 
-const Button = ({ url, context }) => {
+const DEFAULT_STYLE = {
+	display: "inline-block",
+	padding: "0.5rem 1rem",
+	border: "1px solid #ccc",
+	borderRadius: "0.25rem",
+	backgroundColor: "#fff",
+	fontSize: "1rem",
+	fontWeight: "400",
+	lineHeight: "1.5",
+	color: "#212529",
+	textAlign: "center",
+	whiteSpace: "nowrap",
+	verticalAlign: "middle",
+	cursor: "pointer",
+	userSelect: "none",
+};
+
+const Button = ({ url, context, style }) => {
+	const styleMerged = mergeStyle(DEFAULT_STYLE, style);
 	return (
 		<a href={url}>
-			<button>{context}</button>
+			<button style={styleMerged}>{context}</button>
 		</a>
 	);
 };
