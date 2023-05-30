2321a6a5367ea7d0462d26a28b57bd9c23493245
Kousuke <yokosukenji2@gmail.com>
2023-05-30 14:18:24 +0900

    add Text28

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "./components/Button";
 import Table from "./components/Table";
+import Text from "./components/Text";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 			<Button url="https://www.youtube.com/" context="YouTube" style={{ backgroundColor: "blue" }} />
 			<Button url="https://www.twitter.com/" context="Twitter" style={{ backgroundColor: "green" }} />
 			<Table context="banana" />
+			<Text context="こんにちは"/>
 		</>
 	);
 }
