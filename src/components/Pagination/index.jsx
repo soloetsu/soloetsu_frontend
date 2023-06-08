e84fafe13443ef7b4514df8533129c761fddfc30
Kousuke <yokosukenji2@gmail.com>
2023-06-08 14:39:00 +0900

    update　Pagenation

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,45 +1,41 @@
 import React from "react";
 
 const Pagination = () => {
+	const paginationStyle = {
+		display: "flex",
+		justifyContent: "center",
+		marginTop: "20px",
+	};
+
+	const pageStyle = {
+		padding: "5px 10px",
+		margin: "0 5px",
+		backgroundColor: "lightgray",
+		textDecoration: "none",
+		color: "black",
+		transition: "background-color 0.3s, color 0.3s",
+	};
+
+	const handlePageClick = (e) => {
+		e.preventDefault();
+	};
+
 	return (
 		<div>
-			<style>
-				{`
-        .pagination {
-          display: flex;
-          justify-content: center;
-          margin-top: 20px;
-        }
-
-        .page {
-          padding: 5px 10px;
-          margin: 0 5px;
-          background-color: lightgray;
-          text-decoration: none;
-          color: black;
-        }
-
-        .page:hover {
-          background-color: gray;
-          color: white;
-        }
-        `}
-			</style>
-
-			<div className="pagination">
-				<a href="#" className="page">
+			<div style={paginationStyle}>
+				<a href="#" style={pageStyle} onClick={handlePageClick}>
 					1
 				</a>
-				<a href="#" className="page">
+				<a href="#" style={pageStyle} onClick={handlePageClick}>
 					2
 				</a>
-				<a href="#" className="page">
+				<a href="#" style={pageStyle} onClick={handlePageClick}>
 					3
 				</a>
-				<a href="#" className="page">
+				<a href="#" style={pageStyle} onClick={handlePageClick}>
 					4
 				</a>
-				<a href="#" className="page">
+				<a href="#" style={pageStyle} onClick={handlePageClick}>
 					5
 				</a>
 			</div>
