6c2c447a5a08e7d590b9c7594cfc8dd1d8f97866
Kousuke <yokosukenji2@gmail.com>
2023-06-13 14:14:53 +0900

    update Pagenation

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,46 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Pagination = () => {
-	const paginationStyle = {
-		display: "flex",
-		justifyContent: "center",
-		marginTop: "20px",
+function HomePage() {
+	const [currentPage, setCurrentPage] = useState(1);
+
+	// ページネーションの総ページ数
+	const totalPages = 20;
+
+	// ページを変更する関数
+	const changePage = (page) => {
+		setCurrentPage(page);
 	};
 
-	const pageStyle = {
-		padding: "5px 10px",
-		margin: "0 5px",
-		backgroundColor: "lightgray",
-		textDecoration: "none",
-		color: "black",
-		transition: "background-color 0.3s, color 0.3s",
-	};
+	// ページネーションのボタンを生成する関数
+	const renderPageButtons = () => {
+		const buttons = [];
 
-	const handlePageClick = (e) => {
-		e.preventDefault();
+		// 省略記号の表示の閾値
+		const threshold = 2;
+
+		// 先頭のページボタンを表示
+		buttons.push(
+			<button key={1} className={currentPage === 1 ? "active" : ""} onClick={() => changePage(1)}>
+				1
+			</button>
+		);
+
+		if (currentPage > threshold + 2) {
+			buttons.push(
+				<span key="ellipsis1" className="ellipsis">
+					...
+				</span>
+			);
+		}
+
+		// 現在のページを中心に表示する範囲を計算
+		let start = Math.max(currentPage - threshold, 2);
+		let end = Math.min(currentPage + threshold, totalPages - 1);
+
+		for (let page = start; page <= end; page++) {
+			buttons.push(
+				<button key={page} className={currentPage === page ? "active" : ""} onClick={() => changePage(page)}>
+					{page}
+				</button>
+			);
+		}
+
+		if (currentPage < totalPages - threshold - 1) {
+			buttons.push(
+				<span key="ellipsis2" className="ellipsis">
+					...
+				</span>
+			);
+		}
+
+		// 最後のページボタンを表示
+		if (totalPages > 1) {
+			buttons.push(
+				<button
+					key={totalPages}
+					className={currentPage === totalPages ? "active" : ""}
+					onClick={() => changePage(totalPages)}
+				>
+					{totalPages}
+				</button>
+			);
+		}
+
+		return buttons;
 	};
 
 	return (
 		<div>
-			<div style={paginationStyle}>
-				<a href="#" style={pageStyle} onClick={handlePageClick}>
-					1
-				</a>
-				<a href="#" style={pageStyle} onClick={handlePageClick}>
-					2
-				</a>
-				<a href="#" style={pageStyle} onClick={handlePageClick}>
-					3
-				</a>
-				<a href="#" style={pageStyle} onClick={handlePageClick}>
-					4
-				</a>
-				<a href="#" style={pageStyle} onClick={handlePageClick}>
-					5
-				</a>
-			</div>
+			{/* ページのコンテンツを表示 */}
+			<h1>Page {currentPage}</h1>
+			<p>This is the content of page {currentPage}.</p>
+
+			{/* ページネーションのボタンを表示 */}
+			<div className="pagination">{renderPageButtons()}</div>
 		</div>
 	);
-};
+}
 
-export default Pagination;
+export default HomePage;
