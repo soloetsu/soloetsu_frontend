71d753ceeb0f2c017a0ad08c12fb5c366145364d
haminem <ryuu.shushu@gmail.com>
2023-06-14 10:22:36 +0900

    add reset.cssを適用

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "./components/Button";
 import Text from "./components/Text";
 import "./App.css";
+import "./reset.css";
 
 function App() {
 	return (
