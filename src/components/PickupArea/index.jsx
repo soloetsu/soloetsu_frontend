72b7dad504a7b990b234ae20097a6faf46382751
haminem <ryuu.shushu@gmail.com>
2023-06-28 09:59:02 +0900

    update 見出しをheadingに変更

--- src/components/PickupArea/index.jsx
+++ src/components/PickupArea/index.jsx
@@ -1,83 +1,63 @@
 import React from "react";
+import Heading from "@/components/Heading";
 
-const PickupArea = ({ url, img, context }) => {
+const PickupArea = ({ url, img }) => {
 	return (
 		<>
-			<div style={{ marginBottom: "27px", overflow: "hidden" }}>
-				<a>
-					<h1 style={{ margin: 0, textAlign: "center", textDecoration: "underline" }}>{context}</h1>
-				</a>
-			</div>
+			<Heading context="定番のプラン" />
+			<div style={{ display: "flex" }}>
+				<div style={{ marginRight: "38px" }}>
+					<a href={url} style={{ textDecoration: "none" }}>
+						<img src={img} alt="画像1" style={{ maxWidth: "100%", height: "auto", marginBottom: "27px" }} />
+					</a>
+					<a href={url} style={{ textDecoration: "none" }}>
+						<h3
+							style={{
+								textDecoration: "none",
+								margin: 0,
+								textAlign: "center",
+								overflow: "hidden",
+							}}
+						>
+							カテゴリ名1
+						</h3>
+					</a>
+				</div>
 
-			<div>
-				<div style={{ display: "flex" }}>
-					<div style={{ marginRight: "38px" }}>
-						<a href={url} style={{ textDecoration: "none" }}>
-							<img
-								src={img}
-								alt="画像1"
-								style={{ maxWidth: "100%", height: "auto", marginBottom: "27px" }}
-							/>
-						</a>
-						<div style={{ textAlign: "center" }}>
-							<div style={{ width: "max-content" }}>
-								<a href={url} style={{ textDecoration: "none" }}>
-									<h3
-										style={{
-											margin: 0,
-											overflow: "hidden",
-										}}
-									>
-										カテゴリ名1
-									</h3>
-								</a>
-							</div>
-						</div>
-					</div>
+				<div style={{ marginRight: "38px" }}>
+					<a href={url} style={{ textDecoration: "none" }}>
+						<img src={img} alt="画像2" style={{ maxWidth: "100%", height: "auto", marginBottom: "27px" }} />
+					</a>
+					<a href={url} style={{ textDecoration: "none" }}>
+						<h3
+							style={{
+								textDecoration: "none",
+								margin: 0,
+								textAlign: "center",
+								overflow: "hidden",
+							}}
+						>
+							カテゴリ名2
+						</h3>
+					</a>
+				</div>
 
-					<div style={{ marginRight: "38px" }}>
-						<a href={url} style={{ textDecoration: "none" }}>
-							<img
-								src={img}
-								alt="画像2"
-								style={{ maxWidth: "100%", height: "auto", marginBottom: "27px" }}
-							/>
-						</a>
-						<a href={url} style={{ textDecoration: "none" }}>
-							<h3
-								style={{
-									textDecoration: "none",
-									margin: 0,
-									textAlign: "center",
-									overflow: "hidden",
-								}}
-							>
-								カテゴリ名2
-							</h3>
-						</a>
-					</div>
-
-					<div>
-						<a href={url} style={{ textDecoration: "none" }}>
-							<img
-								src={img}
-								alt="画像3"
-								style={{ maxWidth: "100%", height: "auto", marginBottom: "27px" }}
-							/>
-						</a>
-						<a href={url} style={{ textDecoration: "none" }}>
-							<h3
-								style={{
-									textDecoration: "none",
-									margin: 0,
-									textAlign: "center",
-									overflow: "hidden",
-								}}
-							>
-								カテゴリ名3
-							</h3>
-						</a>
-					</div>
+				<div>
+					<a href={url} style={{ textDecoration: "none" }}>
+						<img src={img} alt="画像3" style={{ maxWidth: "100%", height: "auto", marginBottom: "27px" }} />
+					</a>
+					<a href={url} style={{ textDecoration: "none" }}>
+						<h3
+							style={{
+								textDecoration: "none",
+								margin: 0,
+								textAlign: "center",
+								overflow: "hidden",
+							}}
+						>
+							カテゴリ名3
+						</h3>
+					</a>
 				</div>
 			</div>
 		</>
