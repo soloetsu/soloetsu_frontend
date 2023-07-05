f3a9cb54f1ea0a7d08ca8a38c114c3baf63e4fbe
haminem <ryuu.shushu@gmail.com>
2023-07-05 10:16:32 +0900

    add 各パーツをコンポーネントに切り分け

---
+++ src/components/Pagination/Arrow/index.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const Button = ({ style, direction, totalPages, currentPage, setCurrentPage }) => {
+	const DEFAULT_STYLE = style;
+	const HOVER_STYLE = {
+		...DEFAULT_STYLE,
+		backgroundColor: "#eaeaea",
+	};
+
+	const [hovered, setHovered] = useState(false);
+
+	const handleClick = () => {
+		if (direction === "left" && currentPage > 1) {
+			setCurrentPage(currentPage - 1);
+		} else if (direction === "right" && currentPage < totalPages) {
+			setCurrentPage(currentPage + 1);
+		}
+	};
+
+	return (
+		<button
+			style={hovered ? HOVER_STYLE : DEFAULT_STYLE}
+			onMouseEnter={() => setHovered(true)}
+			onMouseLeave={() => setHovered(false)}
+			onClick={handleClick}
+		>
+			{direction === "left" ? "<" : ">"}
+		</button>
+	);
+};
+
+export default Button;
