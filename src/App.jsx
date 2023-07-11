e4dfeb96db40a700e4bd594ca4c06aae8b5ab285
wistream <wisteriastream0406@gmail.com>
2023-07-11 14:32:11 +0900

    add plan

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import TagAreaTest from "./pages/TagAreaTest";
 import PaginationTest from "./pages/PaginationTest";
 import PickupAreaTest from "./pages/PickupAreaTest";
 import BreadCrumbTest from "./pages/BreadcrumbTest";
+import PlanTest from "./pages/PlanTest";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
 				<Route path="/paginationTest" element={<PaginationTest />} />
 				<Route path="/pickupAreaTest" element={<PickupAreaTest />} />
 				<Route path="/breadCrumbTest" element={<BreadCrumbTest />} />
+				<Route path="/planTest" element={<PlanTest />} />
 			</Routes>
 		</Router>
 	);
---
+++ src/components/PlanTraffic/index.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Heading from "@/components/Heading";
+
+const PLAN_STYLE = {
+	display: "flex",
+	flexDirection: "column",
+	width: "60%",
+	margin: "auto",
+	marginBottom: "40px",
+};
+
+const CONTEXT_STYLE = {
+	display: "flex",
+	width: "50%",
+	marginBottom: "65px",
+};
+
+const SPOT_STYLE = {
+	display: "flex",
+};
+
+const BORDER_STYLE = {
+	borderLeft: "6px solid #000000",
+	marginLeft: "3.5%",
+	padding: "5%",
+	height: "20%",
+};
+
+const ICON_STYLE = {
+	width: "92px",
+	height: "92px",
+	marginRight: "68px",
+	backgroundColor: "red",
+};
+
+const PlanTraffic = ({ title, link, type }) => {
+	return (
+		<div style={PLAN_STYLE}>
+			<div style={CONTEXT_STYLE}>
+				<div>
+					{type === "train" ? (
+						<img
+							style={ICON_STYLE}
+							src="https://fujifilmsquare.jp/assets/img/column/column_24_01.jpg"
+							alt="train"
+						/>
+					) : type === "bus" ? (
+						<img
+							style={ICON_STYLE}
+							src="https://fujifilmsquare.jp/assets/img/column/column_24_02.jpg"
+							alt="bus"
+						/>
+					) : (
+						<img
+							style={ICON_STYLE}
+							src="https://fujifilmsquare.jp/assets/img/column/column_24_03.jpg"
+							alt="foot"
+						/>
+					)}
+				</div>
+				<Heading fontSize="MEDIUM" context={title} />
+			</div>
+			<div style={SPOT_STYLE}>
+				<div style={BORDER_STYLE}></div>
+				<p>{link}</p>
+			</div>
+		</div>
+	);
+};
+
+export default PlanTraffic;
