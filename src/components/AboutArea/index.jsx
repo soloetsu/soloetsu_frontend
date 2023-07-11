39b044a8a31961e906da1085fe0a3511dd90ca68
ohagi <krm.0122.963@gmail.com>
2023-07-11 13:48:40 +0900

    update aboutarea

--- src/components/AboutArea/index.jsx
+++ src/components/AboutArea/index.jsx
@@ -1,65 +1,104 @@
 import React from "react";
 import allow from "@/images/AboutArea_yajirusi.png";
 import Heading from "@/components/Heading";
+import FONT_SIZE from "@/utils/fontSize";
+
+const DEFAULT_AREA = {
+	width: "80%",
+	height: "100vh",
+	margin: "auto",
+	display: "flex",
+};
+
+const DEFAULT_LEFT = {
+	width: "50%",
+	display: "flex",
+	marginTop: "60px",
+	marginRight: "30px",
+};
+
+const DEFAULT_RIGHT = {
+	width: "50%",
+};
+
+const DEFAULT_TEXT_AREA = {
+	width: "70%",
+};
+
+const DEFAULT_BIGTEXT_AREA = {
+	width: "30%",
+};
+
+const DEFAULT_BIGTEXT = {
+	fontSize: "70px",
+	color: "#B1C7CF",
+	fontFamily: "'playfair display', serif",
+	transform: "rotate(-90deg)",
+	// transformOrigin: "left bottom",
+	// whiteSpace: "nowrap",
+	margin: "100px 0px 0px -75px",
+	letterSpacing: "20px",
+	width: "fit-content",
+};
+
+const DEFAULT_TEXT = {
+	fontSize: FONT_SIZE["SMALL"],
+	marginTop: "60px",
+	marginBottom: "30px",
+};
+
+const DEFAULT_TEXT_2 = {
+	fontSize: FONT_SIZE["MEDIUM"],
+	marginBottom: "100px",
+};
+
+const DEFAULT_IMG_AREA = {
+	position: "relative",
+	width: "100%",
+	height: "600px",
+	marginTop: "60px",
+	marginLeft: "50px",
+};
+
+const IMG_1 = {
+	position: "absolute",
+	width: "400px",
+	height: "400px",
+	left: "120px",
+	top: "0px",
+	zIndex: "1",
+};
+
+const IMG_2 = {
+	position: "absolute",
+	width: "280px",
+	height: "350px",
+	left: "0px",
+	bottom: "0",
+	zIndex: "2",
+};
 
 const AboutArea = () => {
 	return (
-		<div style={{ display: "flex", alignItems: "center", width: "800px", margin: "0 auto" }}>
-			<div style={{ flex: "1", display: "flex", flexDirection: "row" }}>
-				<div style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
-					<h2
-						style={{
-							fontSize: "70px",
-							color: "#778da9",
-							fontFamily: "",
-							transform: "rotate(-90deg)",
-							transformOrigin: "left bottom",
-							whiteSpace: "nowrap",
-							margin: "100px 0 0 0",
-						}}
-					>
-						ABOUT
-					</h2>
+		<div style={DEFAULT_AREA}>
+			<div style={DEFAULT_LEFT}>
+				<div style={DEFAULT_BIGTEXT_AREA}>
+					<div style={DEFAULT_BIGTEXT}>ABOUT</div>
 				</div>
-				<div style={{ flex: "1", marginLeft: "-200px" }}>
-					<Heading context="上越とソロ旅" />
-					<p style={{ whiteSpace: "pre-wrap", marginTop: "70px", marginBottom: "70px", textSize: "" }}>
-						上越市と~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-						<br />
-						~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-						<br />
-						~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+				<div style={DEFAULT_TEXT_AREA}>
+					<Heading context="上越とソロ旅" fontSize="LARGE" />
+					<p style={DEFAULT_TEXT}>
+						上越市とソロ旅の組み合わせはとても魅力的で多くの人がソロで訪れています。～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～
 					</p>
-					<div>
-						<img src={allow} alt="yajirusi" />
-					</div>
+					<p style={DEFAULT_TEXT_2}>上越の旅、一人ならではの感動を求めて</p>
+					<img src={allow} alt="yajirusi" />
 				</div>
 			</div>
-			<div style={{ position: "relative", width: "350px", height: "600px" }}>
-				<img
-					src="https://picsum.photos/300/300"
-					alt="Image01"
-					style={{
-						position: "absolute",
-						width: "360px",
-						height: "360px",
-						left: "200px",
-						bottom: "100px",
-						zIndex: "1",
-					}}
-				/>
-				<img
-					src="https://picsum.photos/200/250"
-					alt="Image02"
-					style={{
-						position: "absolute",
-						width: "240px",
-						height: "300px",
-						left: "100px",
-						bottom: "0",
-						zIndex: "2",
-					}}
-				/>
+			<div style={DEFAULT_RIGHT}>
+				<div style={DEFAULT_IMG_AREA}>
+					<img src="https://picsum.photos/300/300" alt="Image01" style={IMG_1} />
+					<img src="https://picsum.photos/200/250" alt="Image02" style={IMG_2} />
+				</div>
 			</div>
 		</div>
 	);
