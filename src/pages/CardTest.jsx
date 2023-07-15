a8c27dbf46812f44e581753fed4e84f31fe608a0
ohagi <krm.0122.963@gmail.com>
2023-07-15 17:22:23 +0900

    uppdate cardtestのwidthを変更

--- src/pages/CardTest.jsx
+++ src/pages/CardTest.jsx
@@ -3,12 +3,14 @@ import Card from "@/components/Card";
 
 const CardTest = () => {
 	return (
-		<Card
-			url="https://www.google.com/"
-			img="https://fujifilmsquare.jp/assets/img/column/column_24_01.jpg"
-			context="カード"
-			tags={["#タグ", "#タグタグタグタグタグタグタグタグタグタグタグタグタグタグタグタグタグタグ", "#タグ"]}
-		/>
+		<div style={{ width: "30%" }}>
+			<Card
+				url="https://www.google.com/"
+				img="https://fujifilmsquare.jp/assets/img/column/column_24_01.jpg"
+				context="カード"
+				tags={["#タグ", "#タグタグタグタグタグタグタグタグタグタグタグタグタグタグタグタグタグタグ", "#タグ"]}
+			/>
+		</div>
 	);
 };
 
