cf657a2f875454cf9aea366b13bf6b278ca170fc
haminem <ryuu.shushu@gmail.com>
2023-07-15 14:48:14 +0900

    fix

--- src/pages/ListTest.jsx
+++ src/pages/ListTest.jsx
@@ -4,12 +4,14 @@ import List from "@/components/List";
 const ListTest = () => {
 	return (
 		<List
-			url="https://www.google.com/"
-			img="https://fujifilmsquare.jp/assets/img/column/column_24_01.jpg"
-			context="お酒飲み歩き、上越酒造・ワイナリーツアー"
-			tags={["#お酒", "#趣味"]}
-			description="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-			button="詳細"
+			props={{
+				plan_id: 1,
+				name: "お酒飲み歩き、上越酒造・ワイナリーツアー",
+				description:
+					"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+				tags: ["#お酒", "#趣味"],
+				img: "https://fujifilmsquare.jp/assets/img/column/column_24_01.jpg",
+			}}
 		/>
 	);
 };
