0bf0d2f87b6aa38d72f1724a3c8705116aa29b09
ohagi <krm.0122.963@gmail.com>
2023-07-16 12:48:27 +0900

    update footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MAIN = {
 	width: "100%" /* 画面の幅 */,
-	height: "75vh" /* 画面の高さ */,
+	height: "60vh" /* 画面の高さ */,
 	display: "flex",
 	justifyContent: "center",
 	alignItems: "center",
@@ -15,7 +15,7 @@ const FONT = {
 	alignItems: "center",
 	justifyContent: "center",
 	position: "absolute",
-	top: "59%",
+	top: "48%",
 	left: "50%",
 	transform: "translate(-50%, -50%)",
 	padding: "1px",
@@ -25,10 +25,28 @@ const LINE = {
 	justifyContent: "center",
 	color: "white",
 	position: "absolute",
-	top: "53%",
+	top: "40%",
 	left: "11%",
 	width: "79%",
 	alignItems: "center",
+};
+
+const LOGO = {
+	position: "absolute",
+	top: "22%",
+	left: "51%",
+	transform: "translate(-50%, -50%)",
+	width: "130px",
+};
+
+const MAIN_COVER = {
+	width: "100%",
+	height: "100%",
+	position: "absolute",
+	top: "0%",
+	left: "0%",
+	backgroundColor: "black",
+	opacity: "0.1",
 };
 
 const FooterTest = () => {
@@ -39,6 +57,10 @@ const FooterTest = () => {
 				alt="メイン画像"
 				style={MAIN}
 			/>
+			<div style={MAIN_COVER}></div>
+			<a href="">
+				<img src="./png/penguin03.png" alt="ロゴ" style={LOGO} />
+			</a>
 			<hr style={LINE} />
 			<div>
 				<a style={FONT}>
