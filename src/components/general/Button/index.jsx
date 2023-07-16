4f82792b3812a63b5fefdd9213367b7f215a5657
ohagi <krm.0122.963@gmail.com>
2023-07-17 03:59:59 +0900

    update オレンジのボタン

--- src/components/general/Button/index.jsx
+++ src/components/general/Button/index.jsx
@@ -1,26 +1,66 @@
 import React from "react";
+import SPACING from "@/utils/spacing";
+import FONT_SIZE from "@/utils/fontSize";
 
 const DEFAULT_STYLE = {
-	display: "inline-block",
-	padding: "0.5rem 1rem",
-	border: "1px solid #ccc",
-	borderRadius: "0.25rem",
-	backgroundColor: "#fff",
-	fontSize: "1rem",
-	fontWeight: "400",
-	lineHeight: "1.5",
-	color: "#212529",
-	textAlign: "center",
-	whiteSpace: "nowrap",
-	verticalAlign: "middle",
+	backgroundImage: "linear-gradient(90deg, #FFA800 0%, #FFD600 100%)",
+	borderRadius: "5px",
 	cursor: "pointer",
-	userSelect: "none",
+	width: "130px",
+	height: "50px",
+	marginTop: SPACING["LARGE"],
+	padding: "3px",
+	border: "none",
+};
+
+const INNER = {
+	display: "flex",
+	justifyContent: "space-between",
+	alignItems: "center",
+	padding: "0px 20px 0 23px",
+	fontSize: FONT_SIZE["MEDIUM"],
+	backgroundImage: "linear-gradient(90deg, #FFA800 0%, #FFD600 100%)",
+	color: "white",
+	borderRadius: "3px",
+	cursor: "pointer",
+	height: "100%",
+	marginLeft: "0.2px",
+};
+
+const ARROW_AREA = {
+	backgroundColor: "#fff",
+	width: "20px",
+	height: "20px",
+	borderRadius: "50%",
+	position: "relative",
+};
+
+const ARROW = {
+	width: "8px",
+	height: "8px",
+	borderTop: "2.5px solid #FFA800",
+	borderRight: "2.5px solid #FFA800",
+	boxSizing: "border-box",
+	transform: "rotate(45deg)",
+	position: "absolute",
+	top: "0",
+	left: "4.5px",
+	bottom: "0",
+	margin: "auto",
+	transition: "0.3s",
 };
 
 const Button = ({ url, context }) => {
 	return (
 		<a href={url}>
-			<button style={DEFAULT_STYLE}>{context}</button>
+			<button style={DEFAULT_STYLE}>
+				<div style={INNER}>
+					{context}
+					<div style={ARROW_AREA}>
+						<span style={ARROW}></span>
+					</div>
+				</div>
+			</button>
 		</a>
 	);
 };
