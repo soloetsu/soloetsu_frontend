97de91a9fe77896adeac9e6676843f00b9888bf9
ohagi <krm.0122.963@gmail.com>
2023-07-16 14:14:56 +0900

    update header

--- src/pages/HeaderTest.jsx
+++ src/pages/HeaderTest.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import Header from "@/components/Header";
 
 const HeaderTest = () => {
-	return <Header />;
+	return (
+		<div style={{ backgroundColor: "#93CDFD", height: "100vh" }}>
+			<Header />
+		</div>
+	);
 };
 
 export default HeaderTest;
