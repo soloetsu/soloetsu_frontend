dc4ccef3d647c66291923e3124d4b48569173045
haminem <ryuu.shushu@gmail.com>
2023-07-18 00:01:16 +0900

    update Pickupのデータを取得

--- src/components/PickupArea/index.jsx
+++ src/components/PickupArea/index.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Heading from "@/components/general/Heading";
 import FONT_SIZE from "@/utils/fontSize";
 import "./hover.css";
+import axios from "axios";
+import { Buffer } from "buffer";
+import { Link } from "react-router-dom";
 
 const DEFAULT_AREA = {
 	width: "80%",
@@ -83,28 +86,80 @@ const DEFAULT_BIGTEXT = {
 	right: "9%",
 };
 
-const PickupArea = ({ props }) => {
+const PickupArea = () => {
+	const [name, useName] = useState("");
+	const [imageData, useImageData] = useState(null);
+	useEffect(() => {
+		getPickup();
+	}, []);
+
+	const getPickup = async () => {
+		const res = await axios.get("http://soloetsu.haltokyo.live/api/pickup");
+		useName(res.data[0].name);
+		useImageData(res.data[0].image);
+	};
+
 	return (
 		<div style={DEFAULT_AREA}>
 			<Heading context="定番のプラン" fontSize="LARGE" />
 			<div style={DEFAULT_BOX_AREA}>
-				{props.map((prop, index) => {
-					return (
-						<div style={DEFAULT_BOX} key={index} className="hover">
-							<p style={DEFAULT_TITLE} className="hover_child">
-								{prop.title}
-							</p>
-							<a href={prop.url} style={{ textDecoration: "none" }}>
-								<img src={prop.img} alt="画像1" style={DEFAULT_IMG} />
-							</a>
-							<div style={DEFAULT_NUM_AREA} className="hover_child">
-								<div style={DEFAULT_NUM_TITLE}>PICKUP</div>
-								<p style={DEFAULT_NUM}>{index + 1}</p>
-								<div style={DEFAULT_NUM_ALIGN}></div>
-							</div>
-						</div>
-					);
-				})}
+				<div style={DEFAULT_BOX} className="hover">
+					<p style={DEFAULT_TITLE}>{name}</p>
+					<Link to="/plan" style={{ textDecoration: "none" }}>
+						{imageData ? (
+							<img
+								src={`data:image/jpeg;base64,${Buffer.from(imageData).toString("base64")}`}
+								alt="画像1"
+								style={DEFAULT_IMG}
+							/>
+						) : (
+							<div>Loading...</div>
+						)}
+					</Link>
+					<div style={DEFAULT_NUM_AREA} className="hover_child">
+						<div style={DEFAULT_NUM_TITLE}>PICKUP</div>
+						<p style={DEFAULT_NUM}>01</p>
+						<div style={DEFAULT_NUM_ALIGN}></div>
+					</div>
+				</div>
+				<div style={DEFAULT_BOX} className="hover">
+					<p style={DEFAULT_TITLE}>{name}</p>
+					<Link to="/plan" style={{ textDecoration: "none" }}>
+						{imageData ? (
+							<img
+								src={`data:image/jpeg;base64,${Buffer.from(imageData).toString("base64")}`}
+								alt="画像1"
+								style={DEFAULT_IMG}
+							/>
+						) : (
+							<div>Loading...</div>
+						)}
+					</Link>
+					<div style={DEFAULT_NUM_AREA} className="hover_child">
+						<div style={DEFAULT_NUM_TITLE}>PICKUP</div>
+						<p style={DEFAULT_NUM}>02</p>
+						<div style={DEFAULT_NUM_ALIGN}></div>
+					</div>
+				</div>
+				<div style={DEFAULT_BOX} className="hover">
+					<p style={DEFAULT_TITLE}>{name}</p>
+					<Link to="/plan" style={{ textDecoration: "none" }}>
+						{imageData ? (
+							<img
+								src={`data:image/jpeg;base64,${Buffer.from(imageData).toString("base64")}`}
+								alt="画像1"
+								style={DEFAULT_IMG}
+							/>
+						) : (
+							<div>Loading...</div>
+						)}
+					</Link>
+					<div style={DEFAULT_NUM_AREA} className="hover_child">
+						<div style={DEFAULT_NUM_TITLE}>PICKUP</div>
+						<p style={DEFAULT_NUM}>03</p>
+						<div style={DEFAULT_NUM_ALIGN}></div>
+					</div>
+				</div>
 				<div style={DEFAULT_BACK}></div>
 			</div>
 			<div style={DEFAULT_BIGTEXT}>PICKUP</div>
--- src/pages/PickupAreaTest.jsx
+++ src/pages/PickupAreaTest.jsx
@@ -2,27 +2,7 @@ import React from "react";
 import PickupArea from "@/components/PickupArea";
 
 const PickupAreaTest = () => {
-	return (
-		<PickupArea
-			props={[
-				{
-					title: "酒巡りの旅",
-					url: "https://www.google.com/",
-					img: "https://i.pinimg.com/originals/a7/7e/54/a77e544dd066bacc948763aafd94a056.png",
-				},
-				{
-					title: "酒巡りの旅",
-					url: "https://www.google.com/",
-					img: "https://i.pinimg.com/originals/a7/7e/54/a77e544dd066bacc948763aafd94a056.png",
-				},
-				{
-					title: "酒巡りの旅",
-					url: "https://www.google.com/",
-					img: "https://i.pinimg.com/originals/a7/7e/54/a77e544dd066bacc948763aafd94a056.png",
-				},
-			]}
-		/>
-	);
+	return <PickupArea />;
 };
 
 export default PickupAreaTest;
