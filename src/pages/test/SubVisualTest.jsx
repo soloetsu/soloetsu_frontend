af53e1917672c7be16e874602e2f429297a1d1c2
haminem <ryuu.shushu@gmail.com>
2023-07-19 08:04:09 +0900

    update

--- src/pages/test/SubVisualTest.jsx
+++ src/pages/test/SubVisualTest.jsx
@@ -2,12 +2,7 @@ import React from "react";
 import SubVisual from "@/components/SubVisual";
 
 const SubVisualTest = () => {
-	return (
-		<SubVisual
-			context="プラン一覧"
-			outline="test"
-		/>
-	);
+	return <SubVisual context="プラン一覧" outline="test" />;
 };
 
 export default SubVisualTest;
