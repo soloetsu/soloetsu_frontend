11b5b7dd2afa1a84338a28a7e9cfd34af427e5a8
ohagi <krm.0122.963@gmail.com>
2023-07-19 00:38:01 +0900

    planlistできた

--- src/pages/PlanList.jsx
+++ src/pages/PlanList.jsx
@@ -1,12 +1,34 @@
 import React, { useState, useEffect } from "react";
 import Header from "@/components/Header";
-import Breadcrumb from "@/components/Breadcrumb";
 import SubVisual from "@/components/SubVisual";
 import TagArea from "@/components/TagArea";
 import Card from "@/components/Card";
 import Pagination from "@/components/Pagination";
 import Footer from "@/components/Footer";
 import axios from "axios";
+
+const CONTENT_AREA = {
+	display: "flex",
+	width: "80%",
+	margin: "125px auto 0",
+	justifyContent: "space-between",
+};
+
+const TAGS_LIST = {
+	width: "25%",
+};
+
+const CARD_LIST = {
+	width: "67%",
+	display: "grid",
+	gridTemplateColumns: " repeat(2, 1fr)",
+	gap: "60px 50px",
+};
+
+const PAGINATION_AREA = {
+	margin: "70px 30vw 150px",
+	width: "66%",
+};
 
 const PlanList = () => {
 	const [tags, useTags] = useState([]);
@@ -22,11 +44,34 @@ const PlanList = () => {
 	return (
 		<>
 			<Header />
-			<Breadcrumb pages={["TOP", "プラン一覧"]} />
 			<SubVisual context="プラン一覧" />
-			<div style={{ display: "flex", justifyContent: "space-between" }}>
-				<TagArea width="20%" tags={tags} />
-				<div style={{ width: "30%" }}>
+			<div style={CONTENT_AREA}>
+				<div style={TAGS_LIST}>
+					<TagArea width="100%" tags={tags} margin="0 0 80px 0" />
+					<TagArea width="100%" tags={tags} />
+				</div>
+
+				<div style={CARD_LIST}>
+					<Card
+						id="1"
+						img="https://fujifilmsquare.jp/assets/img/column/column_24_01.jpg"
+						context="カード"
+						tags={[
+							"#タグ",
+							"#タグタグタグタグタグタグタグタグタグタグタグタグタグタグタグタグタグタグ",
+							"#タグ",
+						]}
+					/>
+					<Card
+						id="1"
+						img="https://fujifilmsquare.jp/assets/img/column/column_24_01.jpg"
+						context="カード"
+						tags={[
+							"#タグ",
+							"#タグタグタグタグタグタグタグタグタグタグタグタグタグタグタグタグタグタグ",
+							"#タグ",
+						]}
+					/>
 					<Card
 						id="1"
 						img="https://fujifilmsquare.jp/assets/img/column/column_24_01.jpg"
@@ -39,7 +84,9 @@ const PlanList = () => {
 					/>
 				</div>
 			</div>
-			<Pagination pages={20} />
+			<div style={PAGINATION_AREA}>
+				<Pagination pages={20} />
+			</div>
 			<Footer />
 		</>
 	);
